e6a68965e5101c9835064fc0d22b2d65195b3663
Vilas-cp <vilaspgowda1000@gmail.com>
2025-01-04 00:03:32 +0530

    gsap added

--- nanosoft-app/src/app/indicator/components/Indicators.js
+++ nanosoft-app/src/app/indicator/components/Indicators.js
@@ -1,6 +1,11 @@
-import React from "react";
+"use client";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import { Poppins } from "next/font/google";
 import BlobComp3 from "./Blobcomp3";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 const poppinsFont = Poppins({ subsets: ["latin"], weight: "600" });
 const poppinsFont1 = Poppins({ subsets: ["latin"], weight: "700" });
 const poppinsFont2 = Poppins({ subsets: ["latin"], weight: "500" });
@@ -33,22 +38,51 @@ const Indicators_Data = [
 ];
 
 const Indicators = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const items = containerRef.current.querySelectorAll(".gsap-item");
+
+    items.forEach((item) => {
+      gsap.fromTo(
+        item,
+        { scale: 0.8, opacity: 0 },
+        {
+          scale: 1,
+          opacity: 1,
+          duration: 1,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: item,
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none none",
+          },
+        }
+      );
+    });
+  }, []);
   return (
     <div className="px-[15%] py-[50px]">
-        <BlobComp3/>
+      <BlobComp3 />
       <div className={poppinsFont1.className}>
         <p className="md:text-6xl text-4xl text-black text-center">
           Our <span className="text-[#00c2e0] ">Indicators!</span>
         </p>
       </div>
       <div className="pt-[50px]">
-        <div className="md:flex md:flex-wrap md:justify-center md:gap-4">
+        <div
+          className="md:flex md:flex-wrap md:justify-center md:gap-4"
+          ref={containerRef}
+        >
           {Indicators_Data.map((item, index) => (
             <div
               key={index}
-              className="flex flex-col items-center md:w-[calc(50%-1rem)] pt-5"
+              className="flex flex-col items-center md:w-[calc(50%-1rem)] pt-5 gsap-item"
             >
-              <p className={poppinsFont.className}><span className="">{item.name}</span></p>
+              <p className={poppinsFont.className}>
+                <span className="">{item.name}</span>
+              </p>
               <img
                 src={item.ImgSrc}
                 alt={item.name}

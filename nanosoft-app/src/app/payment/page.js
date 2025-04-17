45f48950175075ad597aa5ed1253b5cfc07c097a
Naman Rajpoot <namanrajpoot@Namans-MacBook-Pro.local>
2025-04-17 11:39:49 +0530

    Bank HDFC

--- nanosoft-app/src/app/payment/page.js
+++ nanosoft-app/src/app/payment/page.js
@@ -19,10 +19,10 @@ const Page = () => {
             <div>
               <div className="shadow-xl w-fit p-[40px] text-[2vh] rounded-xl text-[#6a6a6a] hover:-translate-y-3 transition-transform duration-300 ease-out">
                 <div className="flex justify-center pb-[10px] ">
-                  <img src="/image/axisbank.png" width={250} className="" />
+                  <img src="/image/HDFC.jpg" width={250} className="" />
                 </div>
                 <p className="p-[12px] font-medium">
-                  <span className=" font-bold">Bank Name:-</span> Axis Bank
+                  <span className=" font-bold">Bank Name:-</span> HDFC BANK
                 </p>
                 <p className="p-[12px] font-medium">
                   <span className=" font-bold">Account Name :-</span>{" "}
@@ -30,10 +30,10 @@ const Page = () => {
                 </p>
                 <p className="p-[12px] font-medium">
                   <span className=" font-bold">Account Number :- </span>
-                  924020027157296
+                  50200108149166
                 </p>
                 <p className="p-[12px] font-medium">
-                  <span className=" font-bold">IFSC Code :-</span> UTIB0002661
+                  <span className=" font-bold">IFSC Code :-</span> HDFC0000281
                 </p>
               </div>
               <div className="shadow-xl w-full p-[30px] rounded-xl mt-[50px] hover:-translate-y-3 transition-transform duration-300 ease-out">
@@ -41,7 +41,7 @@ const Page = () => {
               </div>
             </div>
             <div className="shadow-2xl p-[20px] w-fit h-fit rounded-2xl mt-[50px] xl:mt-0 hover:-translate-y-3 transition-transform duration-300 ease-out">
-              <img src="/image/axisqr.jpeg" width={450} />
+              <img src="/image/HDFC name.png" width={450} />
             </div>
           </div>
         </div>
